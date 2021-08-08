1ed1e0fce8cb6cea908c0f0646825d7cf899ee15
V3XL <48788566+V3XL@users.noreply.github.com>
2021-08-08 14:02:02 +1000

    Update update_runemate.sh

--- scripts/update_runemate.sh
+++ scripts/update_runemate.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-mkdir -p '/tmp/RuneMate'
-wget -4 -O /tmp/RuneMate/RuneMate.jar https://www.runemate.com/client/download.php
-wget -4 -O /tmp/RuneMate_setup.sh https://cdn.runemate.com/builds/2.107.1.0/linux/amd64/RuneMate_setup.sh
-chmod +x /tmp/RuneMate_setup.sh
-sh -c 'echo "o\r/usr/local/bin\ry\ry\rn\r\r\r"  | /tmp/RuneMate_setup.sh'
-rm -rf /tmp/RuneMate
+mkdir -p '/tmp/OSBot'
+wget -4 -O /tmp/OSBot/OSBot.jar http://osbot.org/mvc/get
+rm -rf /tmp/OSBot
